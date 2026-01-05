5ca409d8aee237329ce2d509c5545c6bccb877db
Ninja-jr <ninjajr1983@gmail.com>
2026-01-05 04:36:47 +0100

    Update ble_spam.h

--- src/modules/ble/ble_spam.h
+++ src/modules/ble/ble_spam.h
@@ -1,25 +1,14 @@
 #pragma once
+
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEAdvertising.h>
-#include <BLEUtils.h>
+#include <NimBLEBeacon.h>
+#include <NimBLEDevice.h>
+#include <NimBLEServer.h>
+#include <NimBLEUtils.h>
 
-enum EBLEPayloadType { 
-    AppleIOS, Microsoft, SamsungWatch, SamsungBuds, 
-    SamsungRaw, GoogleFastPair, CustomName, NameFlood 
-};
-
-void generateRandomMac(uint8_t *mac);
-const char *generateRandomName();
-void hexStringToBytes(const char* hexString, uint8_t* output, size_t outputLength);
-const char* getRandomBudsId();
-uint8_t* createApplePacket(uint8_t deviceType, bool isContinuity = false);
-bool setRandomBLEAddress();
-BLEAdvertisementData GetUniversalAdvertisementData(EBLEPayloadType Type, int specific_index = -1);
-void executeSpam(EBLEPayloadType type, int delayMs = 20, int specific_index = -1);
-void executeCustomSpam(String spamName, bool isFloodMode = false);
 void aj_adv(int ble_choice);
-
-extern BLEAdvertising *pAdvertising;
-extern const char* flood_names[];
-extern const int FLOOD_NAME_COUNT;
+void ibeacon(
+    const char *DeviceName = "Bruce iBeacon", 
+    const char *BEACON_UUID = "8ec76ea3-6668-48da-9866-75be8bc86f4d",
+    int ManufacturerId = 0x4C00
+);
