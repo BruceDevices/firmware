6d3152e6470d74be750421a3c91490305494316c
Senape3000 <80119773+Senape3000@users.noreply.github.com>
2026-01-06 18:53:37 +0100

    Update rfid_js.h

--- src/modules/bjs_interpreter/rfid_js.h
+++ src/modules/bjs_interpreter/rfid_js.h
@@ -1,5 +1,6 @@
 // Author: Senape3000
 // More info: https://github.com/Senape3000/firmware/blob/main/docs_custom/JS_RFID/RFID_API_README.md
+// More info: https://github.com/Senape3000/firmware/blob/main/docs_custom/JS_RFID/RFID_SRIX_API_README.md
 
 #if !defined(LITE_VERSION) && !defined(DISABLE_INTERPRETER)
 
@@ -19,6 +20,14 @@ duk_ret_t native_rfidLoad(duk_context *ctx);
 duk_ret_t native_rfidClear(duk_context *ctx);
 // MIFARE key management
 duk_ret_t native_rfid_AddMifareKey(duk_context *ctx);
+// SRIX functions
+duk_ret_t native_srixRead(duk_context *ctx);
+duk_ret_t native_srixWrite(duk_context *ctx);
+duk_ret_t native_srixSave(duk_context *ctx);
+duk_ret_t native_srixLoad(duk_context *ctx);
+duk_ret_t native_srixClear(duk_context *ctx);
+duk_ret_t native_srixWriteBlock(duk_context *ctx);
+
 #endif
 
 #endif
