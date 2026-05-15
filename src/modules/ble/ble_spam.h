ceb5e756fd74e9c2bd4e59ef57a3c1573a2bd9a1
MarlinSchuck <Marlinschuck@icloud.com>
2026-05-15 11:59:52 +0200

    Update ble_spam.h

--- src/modules/ble/ble_spam.h
+++ src/modules/ble/ble_spam.h
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 void aj_adv(int ble_choice);
 void spamMenu();
+void legacySubMenu();
 
 void ibeacon(
     const char *DeviceName = "Bruce iBeacon", const char *BEACON_UUID = "8ec76ea3-6668-48da-9866-75be8bc86f4d",
