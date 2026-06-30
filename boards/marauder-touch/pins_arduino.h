ff4850a85643dc4c6fb1ffa32b9ef3e4441c3cd1
Taxi-TH <0630789001a@gmail.com>
2026-07-01 01:25:13 +0700

    Update pins_arduino.h

--- boards/marauder-touch/pins_arduino.h
+++ boards/marauder-touch/pins_arduino.h
@@ -7,115 +7,55 @@
 #define USB_VID 0x303a
 #define USB_PID 0x1001
 
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
 
-static const uint8_t TXD2 = 1;
-static const uint8_t RXD2 = 2;
+#include <stdint.h>
 
-static const uint8_t SDA = 13;
-static const uint8_t SCL = 15;
+static const uint8_t TX = 1;
+static const uint8_t RX = 3;
 
-// Modified elsewhere
-static const uint8_t SS = -1;
-static const uint8_t MOSI = -1;
-static const uint8_t MISO = -1;
-static const uint8_t SCK = -1;
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
 
-static const uint8_t G0 = 0;
-static const uint8_t G1 = 1;
-static const uint8_t G2 = 2;
-static const uint8_t G3 = 3;
-static const uint8_t G4 = 4;
-static const uint8_t G5 = 5;
-static const uint8_t G6 = 6;
-static const uint8_t G7 = 7;
-static const uint8_t G8 = 8;
-static const uint8_t G9 = 9;
-static const uint8_t G10 = 10;
-static const uint8_t G11 = 11;
-static const uint8_t G12 = 12;
-static const uint8_t G13 = 13;
-static const uint8_t G14 = 14;
-static const uint8_t G15 = 15;
-static const uint8_t G39 = 39;
-static const uint8_t G40 = 40;
-static const uint8_t G41 = 41;
-static const uint8_t G42 = 42;
-static const uint8_t G43 = 43;
-static const uint8_t G44 = 44;
-static const uint8_t G46 = 46;
+static const uint8_t SS = 5;
+static const uint8_t MOSI = 23;
+static const uint8_t MISO = 19;
+static const uint8_t SCK = 18;
 
-static const uint8_t ADC1 = 7;
-static const uint8_t ADC2 = 8;
+static const uint8_t A0 = 36;
+static const uint8_t A3 = 39;
+static const uint8_t A4 = 32;
+static const uint8_t A5 = 33;
+static const uint8_t A6 = 34;
+static const uint8_t A7 = 35;
+static const uint8_t A10 = 4;
+static const uint8_t A11 = 0;
+static const uint8_t A12 = 2;
+static const uint8_t A13 = 15;
+static const uint8_t A14 = 13;
+static const uint8_t A15 = 12;
+static const uint8_t A16 = 14;
+static const uint8_t A17 = 27;
+static const uint8_t A18 = 25;
+static const uint8_t A19 = 26;
 
-#define RGB_LED 21
+static const uint8_t T0 = 4;
+static const uint8_t T1 = 0;
+static const uint8_t T2 = 2;
+static const uint8_t T3 = 15;
+static const uint8_t T4 = 13;
+static const uint8_t T5 = 12;
+static const uint8_t T6 = 14;
+static const uint8_t T7 = 27;
+static const uint8_t T8 = 33;
+static const uint8_t T9 = 32;
 
-#define BAD_TX 4
-#define BAD_RX 13
+static const uint8_t DAC1 = 25;
+static const uint8_t DAC2 = 26;
 
-// SERIAL (GPS) dedicated pins
-#define SERIAL_TX 4
-#define SERIAL_RX 13
-#define GPS_SERIAL_TX SERIAL_TX
-#define GPS_SERIAL_RX SERIAL_RX
-#define HAS_BTN 0
-#define BTN_ALIAS "\"Ok\""
-#define BTN_PIN 0
-#define BTN_ACT LOW
-
-#define TXLED -1
-#define LED_ON HIGH
-#define LED_OFF LOW
-
-#define CC1101_GDO0_PIN -1
-#define CC1101_SS_PIN -1
-#define CC1101_MOSI_PIN SPI_MOSI_PIN
-#define CC1101_SCK_PIN SPI_SCK_PIN
-#define CC1101_MISO_PIN SPI_MISO_PIN
-
-#define NRF24_CE_PIN -1
-#define NRF24_SS_PIN -1
-#define NRF24_MOSI_PIN SPI_MOSI_PIN
-#define NRF24_SCK_PIN SPI_SCK_PIN
-#define NRF24_MISO_PIN SPI_MISO_PIN
-
-#define FP 1
-#define FM 2
-#define FG 3
-
-#define HAS_SCREEN 1
-#define ROTATION 3
-#define MINBRIGHT 160
-
-#define USER_SETUP_LOADED 1
-#define ILI9341_DRIVER 1
-#define TFT_MISO 19
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS 17
-#define TFT_DC 16
-#define TFT_RST 5
-#define TFT_BL 32
-#define TFT_BACKLIGHT_ON HIGH
-#define SMOOTH_FONT 1
-#define SPI_FREQUENCY 40000000
-#define SPI_READ_FREQUENCY 16000000
-#define SPI_TOUCH_FREQUENCY 2500000
-#define TOUCH_CS 21
-#define HAS_TOUCH 1
-
-// #define SDCARD_CS 12
-#define SDCARD_SCK 18
-#define SDCARD_MISO 19
-#define SDCARD_MOSI 23
-
-#define GROVE_SDA 33
-#define GROVE_SCL 22
-
-#define SPI_SCK_PIN 18
-#define SPI_MISO_PIN 19
-#define SPI_MOSI_PIN 23
-#define SPI_SS_PIN 1
-
+// Deepsleep
+#define DEEPSLEEP_WAKEUP_PIN 36 // Touch interrupt
+#define DEEPSLEEP_PIN_ACT LOW
 #endif /* Pins_Arduino_h */
+
