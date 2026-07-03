66aea6860e04bdfcce88f5f5477a52ad7e0a36f8
duykhang-droid <duykhangok@gmail.com>
2026-07-03 07:30:32 +0000

    ok

--- src/core/theme.cpp
+++ src/core/theme.cpp
@@ -57,10 +57,9 @@ bool BruceTheme::openThemeFile(FS *fs, String filepath, bool overwriteConfigSett
                 *entry.flag = true;
                 entry.path = _th[entry.key].as<String>();
                 // Pre-cache PNGs into BIN files to avoid runtime decoding and allocations
-                if (path.endsWith(".png") || path.endsWith(".PNG")) { preparePngBin(*fs, path); }
-            } else {
+    
                 log_w("THEME: file not found: %s", entry.key);
-            }
+
         }
     }
 
