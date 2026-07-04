be67f81067cd5ff5965becc457bdd6d7a98b0faa
duykhang-droid <duykhangok@gmail.com>
2026-07-04 14:46:13 +0000

    ggggv

--- src/core/display.cpp
+++ src/core/display.cpp
@@ -765,7 +765,7 @@ tft.setTextSize(selectedTextSize);
 tft.setTextColor(options[index].enabled ? bruceConfig.priColor : TFT_DARKGREY);
 tft.fillRect(6, middle - 2 * LH / 2 - 1, tftWidth - 12, 16, bruceConfig.bgColor);
 String label = options[index].label;
-int maxChars = 15;
+int maxChars = 21;
 if (label.length() > maxChars) {
     label = label.substring(0, maxChars) + "..";
     }
