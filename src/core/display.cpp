6d317ae61713f8a96658476d337d9222618aeceb
duykhang-droid <duykhangok@gmail.com>
2026-07-04 11:57:29 +0000

    okeeee

--- src/core/display.cpp
+++ src/core/display.cpp
@@ -748,10 +748,10 @@ void drawSubmenu(int index, std::vector<Option> &options, const char *title) {
 
 
 // Selected item
-int selectedTextSize = options[index].label.length() <= tftWidth / (LW * FG) + 5 ? FG : FM;
-tft.setTextSize(2);
+int selectedTextSize = options[index].label.length() <= tftWidth / (LW * 2) + 5 ? 2 : 1;
+tft.setTextSize(selectesTextSize);
 tft.setTextColor(options[index].enabled ? bruceConfig.priColor : TFT_DARKGREY);
-tft.fillRect(6, middle - FG * LH / 2 - 1, tftWidth - 12, 16, bruceConfig.bgColor);
+tft.fillRect(6, middle - 2 * LH / 2 - 1, tftWidth - 12, 16, bruceConfig.bgColor);
 tft.drawCentreString(options[index].label, tftWidth / 2, middle - selectedTextSize * LH / 2, SMOOTH_FONT);
 /*
 tft.drawFastHLine(
