c8c295c55abf5823e1b410a89db3e2899f2f6790
Ninja-jr <ninjajr1983@gmail.com>
2026-07-12 13:40:35 +0200

    Add deauth functions and include vector header

--- src/modules/wifi/deauther.h
+++ src/modules/wifi/deauther.h
@@ -2,7 +2,10 @@
 #define WIFI_DEAUTHER_H
 
 #include "scan_hosts.h"
+#include <vector>
 
 void stationDeauth(Host host);
+void deauthAll();
+void deauthTargetList(const std::vector<Host>& targets);
 
 #endif
