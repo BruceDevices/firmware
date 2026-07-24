a1bf4eee296ec3ac294ee7199d0a6fe2683325a7
pr3y <67231282+pr3y@users.noreply.github.com>
2026-07-24 18:07:44 -0300

    1.16 + reaper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	const VERSION = '1.15';
+	const VERSION = '1.16';
 
 	import Card from '$lib/components/Card.svelte';
 	import News from '$lib/components/News.svelte';
